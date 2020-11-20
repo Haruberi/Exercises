ae4bd2aafe17b0f8660305ecda3e5646f8adb4a0
Anna <anna.h.hallberg.92@gmail.com>
2020-11-20 16:38:27 +0100

    First exercise done

--- ExercisesSchool/ExercisesSchool/Program.cs
+++ ExercisesSchool/ExercisesSchool/Program.cs
@@ -6,7 +6,24 @@ namespace ExercisesSchool
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             1. Write a program in C# Sharp to
+            store elements in an array and print it.
+            Test Data:
+            Input 10 elements in the array:
+            element - 0 : 1
+            element - 1 : 1
+            element - 2 : 2
+.......
+            Expected Output :
+            Elements in array are: 1 1 2 3 4 5 6 7 8 9
+             */
+
+            int[] numbers = { 1, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(" " + numbers[i]);
+            }
         }
     }
 }
