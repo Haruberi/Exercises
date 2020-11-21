02146e4c818d1049daa390cc9bad48e3729c8627
Anna <anna.h.hallberg.92@gmail.com>
2020-11-21 13:19:02 +0100

    Second exercise done

--- ExercisesSchool/ExercisesSchool/Program.cs
+++ ExercisesSchool/ExercisesSchool/Program.cs
@@ -19,11 +19,44 @@ namespace ExercisesSchool
             Elements in array are: 1 1 2 3 4 5 6 7 8 9
              */
 
-            int[] numbers = { 1, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            for(int i = 0; i < numbers.Length; i++)
+            /*int[] numbers = { 1, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for (int i = 0; i < numbers.Length; i++)
             {
                 Console.Write(" " + numbers[i]);
+            }*/
+
+            /*
+             2. Write a program in C# Sharp to read n number of values in an array and display it in reverse order. Test Data :
+            Input the number of elements to store in the array :3
+            Input 3 number of elements in the array :
+            element - 0 : 2
+            element - 1 : 5
+            element - 2 : 7
+            Expected Output:
+            The values store into the array are:
+            2 5 7
+            The values store into the array in reverse are :
+            7 5 2*/
+
+            var threeElements = new int[3] { 2, 5, 7 };
+            int[] reversedElements = threeElements;
+            Console.Write("The values store into the array are: ");
+            Console.WriteLine();
+            foreach(var i in threeElements)
+            {
+                Console.Write(" " + i);
+            }
+            Console.WriteLine();
+            Array.Reverse(reversedElements);
+            Console.Write("The values store into the array in reverse are: ");
+            Console.WriteLine();
+            foreach(int i in reversedElements)
+            {
+                Console.Write(" " + i);
+            }
             }
         }
     }
-}
+
+        
+    
